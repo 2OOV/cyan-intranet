cb8a8f5e9c8e566227ae844a64045a6fd6593b52
2OOV <ruaelj2@gmail.com>
2026-09-16 16:38:20 +0900

    Create employees.js

---
+++ js/employees.js
@@ -0,0 +1,154 @@
+const employees = [
+  {
+    name: "강도연",
+    affiliate: "MEDITECH",
+    organization: "CYAN MEDITECH",
+    department: "임상개발팀",
+    position: "PM",
+    extension: "5341"
+  },
+  {
+    name: "강우진",
+    affiliate: "PHARMA",
+    organization: "CYAN PHARMA",
+    department: "글로벌사업본부",
+    position: "과장",
+    extension: "3271"
+  },
+  {
+    name: "김태경",
+    affiliate: "PHARMA",
+    organization: "CYAN PHARMA",
+    department: "연구개발본부",
+    position: "책임연구원",
+    extension: "3537"
+  },
+  {
+    name: "박은호",
+    affiliate: "PHARMA",
+    organization: "CYAN PHARMA",
+    department: "연구개발본부",
+    position: "연구원",
+    extension: "3561"
+  },
+  {
+    name: "백도윤",
+    affiliate: "BIO",
+    organization: "CYAN BIO",
+    department: "플랫폼연구실",
+    position: "선임연구원",
+    extension: "4132"
+  },
+  {
+    name: "서태현",
+    affiliate: "MEDICAL",
+    organization: "CYAN MEDICAL",
+    department: "성하대학교병원 · 응급의학과",
+    position: "",
+    extension: "6423"
+  },
+  {
+    name: "오브이 O.OV",
+    affiliate: "GROUP",
+    organization: "CYAN GROUP",
+    department: "그룹감사실",
+    position: "내부통제",
+    extension: "2101"
+  },
+  {
+    name: "오재현",
+    affiliate: "MEDITECH",
+    organization: "CYAN MEDITECH",
+    department: "AI솔루션팀",
+    position: "주임",
+    extension: "5218"
+  },
+  {
+    name: "윤이안",
+    affiliate: "MEDICAL",
+    organization: "CYAN MEDICAL",
+    department: "성하대학교병원 · 순환기내과",
+    position: "",
+    extension: "6317"
+  },
+  {
+    name: "윤재령",
+    affiliate: "MEDICAL",
+    organization: "CYAN MEDICAL",
+    department: "성하대학교병원 · 진단검사의학부",
+    position: "",
+    extension: "6538"
+  },
+  {
+    name: "윤지혁",
+    affiliate: "GROUP",
+    organization: "CYAN GROUP",
+    department: "그룹전략기획실",
+    position: "차장",
+    extension: "2143"
+  },
+  {
+    name: "윤태혁",
+    affiliate: "PHARMA",
+    organization: "CYAN PHARMA",
+    department: "전략사업본부",
+    position: "본부장",
+    extension: "3203"
+  },
+  {
+    name: "윤 하",
+    affiliate: "GROUP",
+    organization: "CYAN GROUP",
+    department: "브랜드커뮤니케이션팀",
+    position: "",
+    extension: "2176"
+  },
+  {
+    name: "윤서현",
+    affiliate: "PHARMA",
+    organization: "CYAN PHARMA",
+    department: "연구개발본부",
+    position: "선임연구원",
+    extension: "3556"
+  },
+  {
+    name: "정이헌",
+    affiliate: "PHARMA",
+    organization: "CYAN PHARMA",
+    department: "연구개발본부",
+    position: "프로젝트 책임자",
+    extension: "3542"
+  },
+  {
+    name: "차이겸",
+    affiliate: "PHARMA",
+    organization: "CYAN PHARMA",
+    department: "전략사업본부",
+    position: "차장",
+    extension: "3214"
+  },
+  {
+    name: "한동우",
+    affiliate: "PHARMA",
+    organization: "CYAN PHARMA",
+    department: "영업마케팅본부",
+    position: "대리",
+    extension: "3715"
+  },
+  {
+    name: "함해솔",
+    affiliate: "BIO",
+    organization: "CYAN BIO",
+    department: "플랫폼연구실",
+    position: "책임연구원",
+    extension: "4126"
+  },
+  {
+    name: "홍시언",
+    affiliate: "GROUP",
+    organization: "CYAN GROUP",
+    department: "그룹투자전략실",
+    position: "이사",
+    extension: "2118"
+  }
+];
