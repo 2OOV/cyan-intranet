6a6eeb0a7611a13f892e1785cf1e0e1ed08d59a0
2OOV <ruaelj2@gmail.com>
2026-09-17 14:48:51 +0900

    Update profiles.js

--- js/profiles.js
+++ js/profiles.js
@@ -2,5 +2,6 @@ const profileLinks = {
   "차이겸": "https://nifty-weather-a78.notion.site/Profile-EMP-1846281-3d9c1937328780469d73cb427b0d5e2c?pvs=143",
   "윤이안": "https://nifty-weather-a78.notion.site/Profile-EMP-1907193-3dac1937328780e6957dc189671976ab?pvs=143",
   "백도윤": "https://nifty-weather-a78.notion.site/Profile-EMP-1914249-3dac19373287806bb12be3326aff9d86?pvs=143",
-  "정이헌": "https://nifty-weather-a78.notion.site/Profile-CY-P-RD-LBR-2601-3dac19373287809a9827f4ef2ceac595?pvs=143"
+  "정이헌": "https://nifty-weather-a78.notion.site/Profile-CY-P-RD-LBR-2601-3dac19373287809a9827f4ef2ceac595?pvs=143",
+  "함해솔": "https://nifty-weather-a78.notion.site/Profile-EMP-1714286-3dbc19373287808a8a2cdf2c8d4a264f"
 };
